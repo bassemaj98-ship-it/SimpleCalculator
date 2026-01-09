95b19bcb570b7f08dc6d27bae7f43bdab85afabe
basema <bassemaj98@gmail.com>
2026-01-09 23:26:12 +0200

    Convert app to simple Java web server

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 
-FROM eclipse-temurin:17
-WORKDIR /app
-COPY SimpleCalculator.java .
-RUN javac SimpleCalculator.java
-EXPOSE 8080
-CMD ["java", "SimpleCalculator"]
+FROM openjdk:17
 
+WORKDIR /app
+
+COPY src ./src
+
+EXPOSE 8080
+
+RUN javac src/simplecalculator/WebApp.java
+
+CMD ["java", "-cp", "src", "simplecalculator.WebApp"]
---
+++ src/WebApp.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class WebApp {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello from Simple Calculator Web App!";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started on port " + port);
+    }
+}
